fe1a26ed036b6fb01ece66b1db52ab34899acc3e
Domingo José Moronta <domingomoronta@gmail.com>
2020-09-03 14:52:39 -0700

    Set up initial data science folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Using scikitlearn to implement fourr different decision tree models: 2 x entropy & 2 x gini impurity.',
+    author='Domingo Moronta',
+    license='MIT',
+)
